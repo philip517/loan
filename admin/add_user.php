64dd070f90c2255086eaa032e250712cd57e6ebe
Phil ip <71814317+philip517@users.noreply.github.com>
2025-10-08 22:09:54 -0700

    clearing the session start
    clearing session start from the files as it is in the auth_admin file

--- admin/add_user.php
+++ admin/add_user.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require '../config.php'; // include your PDO connection
 
 ?>
--- admin/pending_loans.php
+++ admin/pending_loans.php
@@ -1,7 +1,6 @@
 
 <?php 
 
-session_start();
 require 'auth_admin.php';
 require '../config.php'; // include your PDO connection
 
